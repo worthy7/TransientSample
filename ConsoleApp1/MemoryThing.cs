37e67b542a7db44a1b15bcac7765c4c2ffa375d2
Ian Worthington <worthy.vii@gmail.com>
2022-07-27 14:20:44 +0900

    Add project files.

---
+++ ConsoleApp1/MemoryThing.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class MemoryThing
+    {
+        public MemoryThing()
+        {
+            var memthing = new MemoryCache(new MemoryCacheOptions());
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleApp1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var mt = new MemoryThing();
+        }
+    }
+}
